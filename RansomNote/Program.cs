d75992898737031a8ff15237be7cc72f34298607
vksantoshsunny <vksantoshsunny@gmail.com>
2019-11-23 17:59:43 +1300

    updated with solutions

---
+++ RansomNote/Program.cs
@@ -0,0 +1,85 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Solution
+{
+
+    // Complete the checkMagazine function below.
+    static void checkMagazine(string[] magazine, string[] note)
+    {
+        Dictionary<string, int> magazineLookUp = new Dictionary<string, int>();
+
+        
+        foreach(var mag in magazine)
+        {
+            if (magazineLookUp.ContainsKey(mag))
+            {
+                magazineLookUp[mag]++;
+            }
+            else
+            {
+                magazineLookUp.Add(mag, 1);
+            }
+        }
+
+        Dictionary<string, int> noteLookUp = new Dictionary<string, int>();
+
+        foreach (var noteItem in note)
+        {
+            if (noteLookUp.ContainsKey(noteItem))
+            {
+                noteLookUp[noteItem]++;
+            }
+            else
+            {
+                noteLookUp.Add(noteItem, 1);
+            }
+        }
+
+        if(magazine.Length < note.Length)
+        {
+            Console.WriteLine("No");
+            return;
+        }
+
+
+        foreach(var key in noteLookUp.Keys)
+        {
+            if(magazineLookUp.ContainsKey(key) && magazineLookUp[key] >= noteLookUp[key])
+            {
+                continue;
+            }
+            Console.WriteLine("No");
+            return;
+        }
+
+        Console.WriteLine("Yes");
+
+    }
+
+    static void Main(string[] args)
+    {
+        string[] mn = Console.ReadLine().Split(' ');
+
+        int m = Convert.ToInt32(mn[0]);
+
+        int n = Convert.ToInt32(mn[1]);
+
+        string[] magazine = Console.ReadLine().Split(' ');
+
+        string[] note = Console.ReadLine().Split(' ');
+
+        checkMagazine(magazine, note);
+    }
+}
---
+++ TwoStrings/Program.cs
@@ -0,0 +1,78 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Solution
+{
+
+    // Complete the twoStrings function below.
+    static string twoStrings(string s1, string s2)
+    {
+
+       var s1Array = s1.ToLower().ToCharArray();
+        var s2Array = s2.ToLower().ToCharArray();
+
+        Dictionary<char, int> s1ArrayLookup = new Dictionary<char, int>();
+        Dictionary<char, int> s2ArrayLookup = new Dictionary<char, int>();
+
+
+        foreach (var item in s1Array)
+        {
+           if(! s1ArrayLookup.ContainsKey(item))
+            {
+                s1ArrayLookup.Add(item, 1);
+            }
+
+        }
+
+        foreach (var item in s2Array)
+        {
+            if (!s2ArrayLookup.ContainsKey(item))
+            {
+                s2ArrayLookup.Add(item, 1);
+            }
+
+        }
+
+
+        if(s1ArrayLookup.Keys.Any(x => s2ArrayLookup.Keys.Contains(x)))
+            {
+            return "YES";
+        }
+
+
+        return "NO";
+
+
+    }
+
+    static void Main(string[] args)
+    {
+       
+
+        int q = Convert.ToInt32(Console.ReadLine());
+
+        for (int qItr = 0; qItr < q; qItr++)
+        {
+            string s1 = Console.ReadLine();
+
+            string s2 = Console.ReadLine();
+
+            string result = twoStrings(s1, s2);
+
+            
+        }
+
+
+    }
+}
