b6189de8f91abe4fb15157972285e3f1ea6b4d25
vksantoshsunny <vksantoshsunny@gmail.com>
2019-12-12 20:33:22 +1300

    added sols

---
+++ CountValleys/Program.cs
@@ -0,0 +1,68 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Solution
+{
+
+    // Complete the countingValleys function below.
+    static int countingValleys(int n, string s)
+    {
+
+        int seaLevel = 0;
+        int count = 0;
+        var steps = s.ToCharArray();
+
+        foreach (var step in steps)
+        {
+            bool isBelowSealevel = false;
+            if (seaLevel < 0)
+            {
+                isBelowSealevel = true;
+            }
+
+            if (step == 'U')
+            {
+                seaLevel++;
+            }
+            else
+            {
+                seaLevel--;
+            }
+
+            if (seaLevel == 0 && isBelowSealevel)
+            {
+                count++;
+            }
+            
+        }
+
+        return count;
+    }
+
+    static void Main(string[] args)
+    {
+        //TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        int n = Convert.ToInt32(Console.ReadLine());
+
+        string s = Console.ReadLine();
+
+        int result = countingValleys(n, s);
+
+       /* textWriter.WriteLine(result);
+
+        textWriter.Flush();
+        textWriter.Close();*/
+    }
+}
---
+++ SherlockAnangrams/Program.cs
@@ -0,0 +1,68 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Solution
+{
+
+    // Complete the sherlockAndAnagrams function below.
+    static int sherlockAndAnagrams(string s)
+    {
+        int count = 0;
+        string[] stringList = new string[(s.Length * (s.Length + 1)) / 2];
+        int index = 0;
+        Dictionary<string,int> hash = new Dictionary<string, int>();
+        for (int i = 0; i < s.Length; i++)
+        {
+            for (int j = i; j < s.Length; j++)
+            {
+                stringList[index] = s.Substring(i, j - i + 1);
+                index++;
+            }
+        }
+
+
+
+        Console.WriteLine(stringList.Length);
+
+
+        for (int i = 0; i < stringList.Length; i++)
+        {
+
+        }
+
+
+
+        return hash.Keys.Where(x => hash[x] > 1).Count();
+
+    }
+
+    static void Main(string[] args)
+    {
+        //TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        int q = Convert.ToInt32(Console.ReadLine());
+
+        for (int qItr = 0; qItr < q; qItr++)
+        {
+            string s = Console.ReadLine();
+
+            int result = sherlockAndAnagrams(s);
+
+            //textWriter.WriteLine(result);
+        }
+
+        //textWriter.Flush();
+        //textWriter.Close();
+    }
+}
